719bddc480a582c5c08107aac58d6bcbc437479d
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-11-12 19:21:13 +0600

    adding scroll bar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,12 +7,13 @@ const Home = () => {
 
   // loading data
   const [datas, setDatas] = useState([])
-
   useEffect(() => {
     fetch('products.json')
     .then((res) => res.json())
     .then((data) => setDatas(data))
   },[])
+
+
 
   return (
     <div>
@@ -21,7 +22,7 @@ const Home = () => {
       {/* title end */}
       <div className="flex justify-between h-screen sm:gap-2 md:gap-1 lg:gap-2 xl:gap-3">
         {/* cart-item start */}
-        <div className="w-full sm:w-[70%] grid grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-2 sm:gap-2 md:gap-2 lg:gap-3 xl:gap-2 2xl:gap-3 px-2">
+        <div className="w-full sm:w-[70%] grid grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-2 sm:gap-2 md:gap-2 lg:gap-3 xl:gap-2 2xl:gap-3 px-2 overflow-y-scroll scrollbar-thin scrollbar-thumb-green-500 scrollbar-track-white">
           {
             datas.map((item, index) => <Cart key={index} item={item}></Cart>)
           }
