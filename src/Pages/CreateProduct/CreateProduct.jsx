c4b983e1eda5e673426b727dd2ac01ea69dd88e6
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-11-17 23:32:07 +0600

    changing the createUsers and createProduct form

--- src/Pages/CreateProduct/CreateProduct.jsx
+++ src/Pages/CreateProduct/CreateProduct.jsx
@@ -24,7 +24,7 @@ const CreateProduct = () => {
     <Title name={'Create-Product'}></Title>
       {/* title end */}
       {/* content start */}
-      <div className="container mx-auto flex flex-col justify-center items-center h-full md:h-screen px-5 my-10 md:my-0 gap-5">
+      <div className="container mx-auto sm:flex sm:flex-col items-center h-full md:h-screen px-5 mt-5 md:mt-3 lg:mt-5 xl:mt-6 2xl:mt-8 gap-5">
         {/* form title start */}
         <div className="w-[58%] sm:w-[32%] md:w-[23%] lg:w-[21%] xl:w-[20%] 2xl:w-[20%] mx-auto">
             <h3 className="font-bold text-xl sm:text-2xl md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl text-center border-b-2 border-b-red-500 text-red-500">Create-Product</h3>
@@ -32,26 +32,26 @@ const CreateProduct = () => {
           {/* form title end */}
         {/* form start */}
         <div className="card bg-base-100 w-full sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-7/12 shrink-0 shadow-2xl">
-      <form onSubmit={createProduct} className="card-body">
+      <form onSubmit={createProduct} className="p-3">
         <div className="flex flex-col sm:flex-row justify-between gap-1 sm:gap-3">
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Name</span>
             </label>
-            <input type="text" name="name" placeholder="enter product name" className="input input-bordered input-sm sm:input-md" required />
+            <input type="text" name="name" placeholder="enter product name" className="input input-bordered input-sm md:input-md" required />
           </div>
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Price</span>
             </label>
-            <input type="text" name="price" placeholder="enter product price" className="input input-bordered input-sm sm:input-md" required />
+            <input type="text" name="price" placeholder="enter product price" className="input input-bordered input-sm md:input-md" required />
           </div>
         </div>
         <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Image</span>
             </label>
-            <input type="file" name="image" className="file-input w-full file-input-bordered file-input-sm sm:file-input-md" required/>
+            <input type="file" name="image" className="file-input w-full file-input-bordered file-input-sm md:file-input-md" required/>
         </div>
         <div className="form-control w-full">
             <label className="label">
--- src/Pages/CreateUsers/CreateUsers.jsx
+++ src/Pages/CreateUsers/CreateUsers.jsx
@@ -52,7 +52,7 @@ const CreateUsers = () => {
     <Title name={'Create-Product'}></Title>
       {/* title end */}
       {/* content start */}
-      <div className="container mx-auto flex flex-col justify-center items-center h-full md:h-screen px-5 my-10 md:my-0 gap-5">
+      <div className="container mx-auto sm:flex sm:flex-col items-center h-full md:h-screen px-5 mt-5 md:mt-3 lg:mt-5 xl:mt-6 2xl:mt-8 gap-5">
         {/* form title start */}
         <div className="w-[58%] sm:w-[32%] md:w-[23%] lg:w-[21%] xl:w-[20%] 2xl:w-[20%] mx-auto">
             <h3 className="font-bold text-xl sm:text-2xl md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl text-center border-b-2 border-b-red-500 text-red-500">Create-Users</h3>
@@ -60,22 +60,22 @@ const CreateUsers = () => {
           {/* form title end */}
         {/* form start */}
         <div className="card bg-base-100 w-full sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-7/12 shrink-0 shadow-2xl">
-      <form onSubmit={createProduct} className="card-body">
+      <form onSubmit={createProduct} className="p-5">
         <div className="flex flex-col sm:flex-row justify-between gap-1 sm:gap-3">
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Name</span>
             </label>
-            <input type="text" name="name" placeholder="enter product name" className="input input-bordered input-sm sm:input-md" required />
+            <input type="text" name="name" placeholder="enter product name" className="input input-bordered input-sm md:input-md" required />
           </div>
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
-            <input type="email" name="email" placeholder="enter your email" className="input input-bordered input-sm sm:input-md" required />
+            <input type="email" name="email" placeholder="enter your email" className="input input-bordered input-sm md:input-md" required />
           </div>
         </div>
-        <div className="flex flex-col sm:flex-row justify-between gap-1 sm:gap-3">
+        <div className="flex flex-row justify-between gap-2 sm:gap-3">
           <div className="form-control w-full">
             <label className="label" >
               <span className="label-text">Status</span>
@@ -99,7 +99,7 @@ const CreateUsers = () => {
             <label className="label">
               <span className="label-text">Password</span>
             </label>
-            <input type="password" name="password" placeholder="enter product password" className="input input-bordered input-sm sm:input-md" required />
+            <input type="password" name="password" placeholder="enter product password" className="input input-bordered input-sm md:input-md" required />
           </div>
         <div className="form-control mt-3">
           <button className="py-2 w-full bg-green-500 hover:bg-hover-500 text-white font-bold rounded-md">Create</button>
