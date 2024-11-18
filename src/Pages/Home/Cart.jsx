7cc498e3ccddd6c5f4b6979180f130664b098d41
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-11-18 22:35:46 +0600

    adding some data

--- src/Pages/Home/Cart.jsx
+++ src/Pages/Home/Cart.jsx
@@ -14,7 +14,7 @@ const Cart = ({item, addData,quantityData}) => {
         <li className="text-[10px] sm:text-[10px] md:text-[10px] lg:text-[11px] xl:text-[11px] 2xl:text-[10px] font-bold flex justify-between items-center"><h4>{item.productName}</h4>
         <p>${item.price}</p>
         </li>
-        <li><h4 className="text-[9px] sm:text-[9px] md:text-[9px] lg:text-[9px] xl:text-[10px] 2xl:text-[10px] text-gray-400 font-bold">{item.details}</h4></li>
+        <li><h4 className="text-[9px] sm:text-[9px] md:text-[9px] lg:text-[9px] xl:text-[10px] 2xl:text-[10px] text-gray-400 font-bold">{item.details.slice(0,22)}</h4></li>
         <li><input type="number" className="w-full border-[1px] border-gray-200 focus:outline-none" onChange={() => quantityData(event.target.value)}></input></li>
         <li><button onClick={() => addData(item)} className="text-xs bg-green-500 py-1 w-full font-bold text-white rounded-md">Add</button></li>
       </ul>
